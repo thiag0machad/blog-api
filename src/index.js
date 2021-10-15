bc54984918d341119ba4f91127be11c9e7abda13
Thiago Machado <thcosta0@hotmail.com>
2021-10-15 15:12:09 -0300

    Curso iniciado conhecendo a introspecção do Prisma 2

--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
 const { ApolloServer, gql } = require('apollo-server')
 
 const typeDefs = gql`
- type User {
-   nome: String
- }
- type Query {
-   users: [User]
- }
+  type User {
+    nome: String
+  }
+  type Query {
+    users: [User]
+  }
 `
 
 const resolvers = {
- Query: {
-   users: () => [{ nome: "Ana" }, {nome: "Bia" }]
- }
+  Query: {
+    users: () => [{ nome: 'Ana' }, { nome: 'Bia' }],
+  },
 }
 
 const server = new ApolloServer({ typeDefs, resolvers })
-server.listen({ port: 4000 }, () => console.log(`Servidor pronto em localhost:4000`)) 
+server.listen({ port: 4000 }, () =>
+  console.log(`Servidor pronto em localhost:4000`)
+)
